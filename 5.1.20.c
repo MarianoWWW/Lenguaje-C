70627c393fb86ce7b5174035071c1d33572b9cca
MarianoWWW <marianoworoszylo@hotmail.com>
2024-07-19 11:10:55 -0300

    Add files via upload

---
+++ 5.1.20.c
@@ -0,0 +1,33 @@
+/*
+5.1.20 Ingresar un número entero mayor a 0. Debe dibujar una pirámide con * con tantas filas como el número
+indicado. En cada fila se va incrementando la cantidad de asteriscos de 2 en 2.
+Por ejemplo, si se ingresa el número 5 debe mostrar en pantalla:
+     *
+    ***
+   *****
+  *******
+ *********
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int I,J,K,NUM;
+    printf("\n\nIngresar un numero entero mayor a 0: ");
+    scanf ("%d", &NUM);
+
+    for ( I=1 ; I<=NUM*2 ; I=I+2)
+    {
+        printf ("\n");
+        for (K=NUM; K>=I/2 ; K=K-1)
+            printf (" ");
+        for (J=1 ; J<=I ; J=J+1)
+            printf ("*");
+    }
+
+
+    printf("\n\n");
+    system ("pause");
+    return 0;
+}
